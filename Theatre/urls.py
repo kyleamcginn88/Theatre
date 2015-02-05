6d618802ffb25b155f88eb4879b7c55db220ceb9
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-02-05 13:26:07 -0700

    Added admin views urls

--- Theatre/urls.py
+++ Theatre/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from views import home, store, location, movies, performances
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Theatre.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', home),
+	url(r'^movies$', 'Theatre_CS399.views.movies', name='movies'),
+	url(r'^performances$', 'Theatre_CS399.views.performances', name='performances'),
+	url(r'^store$', 'Theatre_CS399.views.store', name='store'),
+	url(r'^location$', 'Theatre_CS399.views.location', name='location'),
+	
     url(r'^admin/', include(admin.site.urls)),
 )
